c207d1f50b15e499b26ddcaf5e8cab0b290a4adf
Vadim Semenov <gosusty@mail.ru>
2018-12-19 00:36:18 +0200

    update

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,42 @@
 import React from 'react';
+import data from '../Data/trading-hours'
 
 export default class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            tradingHoursList: []
+        };
+    };
+
+    getTradingHoursList = () => {
+        this.setState({
+            tradingHoursList: []
+        });
+
+        // const link='./trading-hours.json';
+
+        fetch(data)
+            .then(response => {
+                return response.json();
+            })
+            .then(result => {
+                    this.setState({
+                        tradingHoursList: result
+                    })
+                }
+            )
+    };
+
+
+    componentDidMount() {
+        this.getTradingHoursList()
+    };
+
     render() {
+        console.log(data);
         return (
-            <div className='wrapper'>
+            <div>
                 hello
             </div>
         )
